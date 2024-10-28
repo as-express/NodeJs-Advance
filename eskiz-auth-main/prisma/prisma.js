c1f342683a7c53bf86ac20d00eaf7487605a88df
Aset Yergashev <aset16006@icloud.com>
2024-10-28 22:35:17 +0500

    Delete eskiz-auth-main directory

--- eskiz-auth-main/prisma/prisma.js
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-import {configureDeleteScheduler} from '../services/schedule.service.js'
-
-
-const prisma = new PrismaClient()
-configureDeleteScheduler(prisma)
-
-
-export default prisma
